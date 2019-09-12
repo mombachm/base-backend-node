e0332cb5122f98284d31e1c69e0407c192a9da39
Matheus Mombach <mombachm@gmail.com>
2019-09-12 01:44:08 -0300

    implement deleteDonation route

--- src/routingControllers/DonationController.ts
+++ src/routingControllers/DonationController.ts
@@ -6,7 +6,8 @@ export class DonationController extends RoutingController {
 
   protected assembleRoutes(): void {
     this.router.get("/donations", this.getAllDonations);
-    this.router.get("/createDonation", this.createDonation);
+    this.router.post("/createDonation", this.createDonation);
+    this.router.post("/deleteDonation", this.deleteDonation);
   }
 
   public async getAllDonations(req: express.Request, res: express.Response): Promise<void> {
@@ -15,11 +16,13 @@ export class DonationController extends RoutingController {
   }
 
   public async createDonation(req: express.Request, res: express.Response): Promise<void> {
-    const donation = new Donation({
-      description: "My first donation."
-    });
-
+    const donation = new Donation(req.body);
     const newDonation = await donation.save();
     res.json(newDonation);
+  }
+
+  public async deleteDonation(req: express.Request, res: express.Response): Promise<void> {
+    const deletedDonation = await Donation.findByIdAndRemove(req.body.id);
+    res.json(deletedDonation);
   }
 }
